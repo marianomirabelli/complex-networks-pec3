c9e46976750332e827519ac49d125ef4f6dbea80
Mariano <mariano2106@gmail.com>
2019-05-09 16:40:24 -0300

    feature: calculates indices with radatool

--- radatools/Network_Tools/Multiplex_Extract_Layers.sh
+++
@@ -1,4 +0,0 @@
-echo -----------
-./Multiplex_Extract_Layers.exe  test-multiplex_toy_01.txt  test-multiplex_toy_01  undirected
-./Multiplex_Extract_Layers.exe  test-multiplex_toy_02.txt  test-multiplex_toy_02  directed
-echo -----------
--- radatools/Network_Tools/Net_To_List.sh
+++
@@ -1,4 +0,0 @@
-echo -----------
-./Net_To_List.exe  test-net_toy_01.net  test-net_toy_01-list.txt
-./Net_To_List.exe  test-net_toy_02.net  test-net_toy_02-list.txt
-echo -----------

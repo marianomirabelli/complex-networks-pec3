72dc5df2a22620fc8a6a64e3d93b970464154b9b
Mariano <mariano2106@gmail.com>
2019-05-11 15:12:43 -0300

    feature: spectral modularity algorithm

---
+++ radatools/Communities_Detection/Communities_Detection.sh
@@ -0,0 +1,4 @@
+echo -----------
+./Communities_Detection.exe  v  WN el-lr-ll-srfr-trfr   5  test-zachary_unwh.net  test-zachary_unwh-lol.txt
+./Communities_Detection.exe  v  WN rt-llt-ert+srt.ft!trfr     10  test-dolphins.net  test-dolphins-lol.txt
+echo -----------
